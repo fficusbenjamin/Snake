8e37336ff3ac7d5bcad24dec9af9baf7ca134da5
Francesco Fico <fficusbenjamin@gmail.com>
2019-03-02 18:46:18 +0000

    Initial commit

---
+++ app/src/main/java/napier/coursework/snake/MainActivity.java
@@ -0,0 +1,36 @@
+package napier.coursework.snake;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button btnPlay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnPlay = (Button) findViewById(R.id.btnPlay);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SnakeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
+
+
+    }
+}
