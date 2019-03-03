dd20c5ba6c13e0a56ff50cdfd1db710413580245
Francesco Fico <fficusbenjamin@gmail.com>
2019-03-03 17:03:33 +0000

    HighScoreActivity redifined

--- app/src/main/java/napier/coursework/snake/HighScoreActivity.java
+++ app/src/main/java/napier/coursework/snake/HighScoreActivity.java
@@ -1,11 +1,8 @@
 package napier.coursework.snake;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class HighScoreActivity extends AppCompatActivity {
 
@@ -16,14 +13,9 @@ public class HighScoreActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+
+
     }
 
 }
